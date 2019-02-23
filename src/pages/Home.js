3880b007bf50d983df38b954242933920b1011b0
Lamuel27 <samrelliner@gmail.com>
2019-02-23 17:39:59 -0600

    auth0 is working

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+class Home extends Component {
+render() {
+    return (
+        <div>
+            <h3>This is the Home page</h3>
+        </div>
+        
+    )
+}
+}
+
+export default Home;
---
+++ src/pages/Quiz.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+class Quiz extends Component {
+render() {
+    return (
+        <div>
+            <h3>This is the Quiz page</h3>
+        </div>
+    )
+    
+}
+}
+
+export default Quiz;
