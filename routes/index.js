7198710a6f03560b5bacdc0285029a84ae16c612
Relliner <41902228+rehlingera@users.noreply.github.com>
2019-02-27 21:12:31 -0600

    converted file tree to mvc paradigm

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./score");
+
 // score routes
 router.use("/score", apiRoutes);
+
 // If no score routes are hit, send the React app
 router.use((req, res) =>
   res.sendFile(path.join(__dirname, "../client/build/index.html"))
