d7048ad5401b174139782364af939b5fa836a481
Lamuel27 <samrelliner@gmail.com>
2019-03-01 19:35:52 -0600

    learning resources

---
+++ client/src/components/Companies.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+class Companies extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+            <a href ="https://www.pluralsight.com" target="_blank">
+            <img src = "https://www.pluralsight.com/content/dam/pluralsight/newsroom/brand-assets/logos/PS_logo_F-04.png"></img>
+            </a>
+            <a href ="https://www.w3schools.com/" target="_blank">
+            <img src = "https://ya-webdesign.com/images/w3school-svg.png"></img>
+            </a>
+            <a href ="https://stackoverflow.com/" target="_blank">
+            <img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Stack_Overflow_logo.svg/2000px-Stack_Overflow_logo.svg.png"></img>
+            </a>
+            <a href ="https://codepen.io/" target="_blank">
+            <img src = "https://blog.codepen.io/wp-content/uploads/2012/06/codepen-wordmark-display-inside-black@10x.png"></img>
+            </a>
+            <a href ="https://www.udemy.com/?utm_source=adwords-brand&utm_medium=udemyads&utm_campaign=NEW-AW-PROS-Branded-Search-World-EN-ENG_._ci__._sl_ENG_._vi__._sd_All_._la_EN_._&tabei=7&utm_term=_._ag_48933380294_._ad_279519253626_._de_c_._dm__._pl__._ti_aud-325382095121:kwd-310556426868_._li_9019572_._pd__._&gclid=Cj0KCQiAzePjBRCRARIsAGkrSm6uD4dvWWPufk2fADDfadzdmgs1ZooYrRol4VbGTtSd_7jFhdXePE4aAg-1EALw_wcB" target="_blank">
+            <img src = "https://cdn.freebiesupply.com/logos/large/2x/udemy-2-logo-png-transparent.png"></img>
+            </a>
+            <br/>
+            <a href ="https://www.codecademy.com/?utm_source=google&utm_medium=cpc&utm_campaign=US_Brand_Core_Exact&utm_content=US_Brand_Core_Exact&utm_term=codecademy&d=c&gclid=Cj0KCQiAzePjBRCRARIsAGkrSm5Qv9kUB4ukXh6DocEtmsAJAThpI_kTsNvqkTGdJIT2M2vOVzCeEFEaAhNeEALw_wcB" target="_blank">
+            <img src = "http://s3.amazonaws.com/codecademy-blog/assets/logo_blue_dark.png"></img>
+            </a>
+            <a href ="https://teamtreehouse.com/" target="_blank">
+            <img src = "https://blog.teamtreehouse.com/wp-content/uploads/2017/06/Treehouse-Logo-Green-Medium.png"></img>
+            </a>
+            <a href ="https://developer.mozilla.org/en-US/" target="_blank">
+            <img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/MDN_Web_Docs.svg/1024px-MDN_Web_Docs.svg.png"></img>
+            </a>
+            <a href ="https://www.codewars.com/" target="_blank">
+            <img src = "https://www.qualified.io/shared/images/codewars-black-large-24a9d355.png"></img>
+            </a>
+            <a href ="https://www.lynda.com/" target="_blank">
+            <img src = "https://www.wppl.org/sites/default/files/sites/default/files/lynda_logo3k-p_2x-600x171_0.png"></img>
+            </a>
+            </div>
+          );
+        }
+    }
+
+export default Companies;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import Jumbotron from '../../components/Jumbotron'
+import Jumbotron from '../../components/Jumbotron';
+import Companies from '../../components/Companies';
+import './Home.css';
+
 
 class Home extends Component {
   render() {
     const { isAuthenticated, login, getExpiryDate } = this.props.auth;
     return (
+      <div>
         <Jumbotron />
-        
+        <h2 style = {{ "textAlign":"center" }}>Other great learning resources:</h2>
+        <Companies />
+        </div>
     );
   }
 }
