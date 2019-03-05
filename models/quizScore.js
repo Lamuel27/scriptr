99bd83225e490067b4d3031e043f12586742b230
Lamuel27 <samrelliner@gmail.com>
2019-03-05 10:15:11 -0600

    seed/model work

--- models/quizScore.js
+++ models/quizScore.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const quizSchema = new Schema({
-  id: {type: Number},
-  category: {type: String},
-  questions: {type: Array},
-  correctIndex: {type: Number}
+  
+    category: {type: String},
+    question: {type: String},
+    answers: {type: Array},
+    correctIndex: {type: Number}
+
 });
 
 const Quiz = mongoose.model("quiz", quizSchema);
