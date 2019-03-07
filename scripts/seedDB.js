4e95b40fb4eb2d7660d14a2d039006316cf86370
Relliner <41902228+rehlingera@users.noreply.github.com>
2019-03-06 18:30:43 -0600

    fixed db issue

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const db = require("../models/quizScore");
+const db = require("../models");
 
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/scriptr"
