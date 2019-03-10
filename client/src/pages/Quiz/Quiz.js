12b7f87778fb760215b3de3a01f68dc0b69a5921
Relliner <41902228+rehlingera@users.noreply.github.com>
2019-03-10 17:58:43 -0500

    some quiz scoring logic added. having trouble getting the logic to fire on answer click

--- client/src/pages/Quiz/Quiz.js
+++ client/src/pages/Quiz/Quiz.js
@@ -1,74 +1,131 @@
 import React, { Component } from "react";
 import QuizQuestion from '../../components/QuizQuestion';
 import "./Quiz.css";
-import M from "materialize-css";
 import API from "../../utils/API";
 import { Link } from 'react-router-dom';
 import Activities from '../../components/Activity';
-
+import QuizAnswer from '../../components/QuizAnswer';
 
 class Quiz extends Component {
 
-    style = {
-        linkStyle: {
-            "textDecoration": "none"
+  style = {
+    linkStyle: {
+      "textDecoration": "none"
+    }
+  }
+
+  state = {
+    questions: [],
+    answers: []
+  }
+
+  getData = type => {
+    console.log("boosh")
+    this.setState({ answers: [] })
+    API.getTypeQuestions(type)
+      .then(data => {
+        var questions = [];
+        console.log("Questions:");
+        console.log(data);
+        for (var i = 0; i < 10; i++) {
+          var randomInt = Math.floor(Math.random() * (data.data.length))
+          var randomQ = {};
+          console.log(randomInt)
+          randomQ = data.data[randomInt];
+          console.log(randomQ)
+          questions.push(randomQ);
+          data.data.splice(randomInt, 1);
         }
-    }
+        console.log(questions);
+        this.setState({ questions: questions })
+      })
+      .catch(err => console.log(err));
+  }
 
-    state = {
-        questions: []
+  answerHandler = (id, index) => {
+    console.log("boosh")
+    var answers = (this.state.answers)
+    var answer = {
+      answerId: id,
+      answerIndex: index
     }
+    answers.push(answer);
+    this.setState({ answers: answers })
+    console.log("Answers: ");
+    console.log(this.state.answers);
+  }
 
-    getData(type) {
-        console.log("boosh")
-        API.getTypeQuestions(type)
-          .then(data => {
-            console.log("Questions:");
-            console.log(data);
-            this.setState({ questions: data.data })
-          })
-          .catch(err => console.log(err));
+  evaulate = event => {
+    event.preventDefault();
+    var correct = 0;
+    var total = 0;
+    for (var i = 0; i < this.state.questions.length; i++) {
+      var question = this.state.questions[i];
+      var answer = this.state.answers.filter(item => item.answerId === question._id);
+      if (question.correctIndex === answer.answerIndex) {
+        correct++;
       }
-      componentDidMount() {
-      };
+      total++;
+    }
+    var score = correct/total;
+    console.log("The user's score is " + score*10 + "%")
+  }
 
-    render() {
-        return (
-            <div>
-                <h1 style={{ "textAlign": "center", "color": "white" }}>Take a Quiz</h1>
+  render() {
+    return (
+      <div>
+        <h1 style={{ "textAlign": "center", "color": "white" }}>Take a Quiz</h1>
         <Activities>
-
           <Link to={"/quiz/HTML"} onClick={this.getData.bind(this, "HTML")}>
-            <img className="zoom" style = {{ "width":"17%" }} src="http://www.stickpng.com/assets/images/5847f5bdcef1014c0b5e489c.png" alt=""></img>
+            <img className="zoom" style={{ "width": "17%" }} src="http://www.stickpng.com/assets/images/5847f5bdcef1014c0b5e489c.png" alt=""></img>
           </Link>
           <Link to={"/quiz/CSS"} onClick={this.getData.bind(this, "CSS")}>
-            <img className="zoom" style = {{ "width":"17%" }} src="https://cdn.freebiesupply.com/logos/large/2x/css3-logo-png-transparent.png" alt=""></img>
+            <img className="zoom" style={{ "width": "17%" }} src="https://cdn.freebiesupply.com/logos/large/2x/css3-logo-png-transparent.png" alt=""></img>
           </Link>
           <Link to={"/quiz/JS"} onClick={this.getData.bind(this, "JS")}>
-            <img className="zoom" style = {{ "width":"20%" }} src="https://fuzati.com/wp-content/uploads/2016/12/JS-Logo.png" alt=""></img>
+            <img className="zoom" style={{ "width": "20%" }} src="https://fuzati.com/wp-content/uploads/2016/12/JS-Logo.png" alt=""></img>
           </Link>
           <Link to={"/quiz/SQL"} onClick={this.getData.bind(this, "SQL")}>
-            <img className="zoom" style = {{ "width":"20%" }} src="https://clipart.info/images/ccovers/1499794875MySQL-logo-png-transparent.png" alt=""></img>
+            <img className="zoom" style={{ "width": "20%" }} src="https://clipart.info/images/ccovers/1499794875MySQL-logo-png-transparent.png" alt=""></img>
           </Link>
           <Link to={"/quiz/MONGO"} onClick={this.getData.bind(this, "MONGO")}>
-            <img className="zoom" style = {{ "width":"24%" }} src="http://pluspng.com/img-png/logo-mongodb-png-file-mongodb-logo-svg-1280.png" alt=""></img>
+            <img className="zoom" style={{ "width": "24%" }} src="http://pluspng.com/img-png/logo-mongodb-png-file-mongodb-logo-svg-1280.png" alt=""></img>
           </Link>
-
-
-
         </Activities>
-                {this.state.questions.map(item => (
-                    <QuizQuestion
-                        question={item.question}
-                        a={item.answers[0]}
-                        b={item.answers[1]}
-                        c={item.answers[2]}
-                        d={item.answers[3]}
-                    />
-                ))}
-            </div>
-        )
-    }
+        {this.state.questions.map(item => (
+          <QuizQuestion
+            key={item._id}
+            question={item.question}>
+            <QuizAnswer
+              id={item._id}
+              onClick={() => this.answerHandler(item._id, 0)}
+            >
+              {item.answers[0]}
+            </QuizAnswer>
+            <QuizAnswer
+              id={item._id}
+              onClick={() => this.answerHandler(item._id, 1)}
+            >
+              {item.answers[1]}
+            </QuizAnswer>
+            <QuizAnswer
+              id={item._id}
+              onClick={() => this.answerHandler(item._id, 2)}
+            >
+              {item.answers[2]}
+            </QuizAnswer>
+            <QuizAnswer
+              id={item._id}
+              onClick={() => this.answerHandler(item._id, 3)}
+            >
+              {item.answers[3]}
+            </QuizAnswer>
+          </QuizQuestion>
+        ))}
+        <button id="submitButton" className="waves-effect waves-light btn" onClick={() => this.evaluate}>Submit</button>
+      </div>
+    )
+  }
 }
 
 export default Quiz;
