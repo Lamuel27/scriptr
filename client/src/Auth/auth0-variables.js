7fd57115a8b4547f4453ffa2625780490df5b849
Lamuel27 <samrelliner@gmail.com>
2019-03-15 00:31:12 -0500

    heroku callback

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,7 +1,7 @@
 export const AUTH_CONFIG = {
     domain: 'dev-7isj8d8k.auth0.com',
     clientId: 'En4O0WsdLmYGFhPuz_WxWl-GRR7My0xD',
-    callbackUrl: 'http://localhost:3000/callback'
+    callbackUrl: 'https://powerful-chamber-64882.herokuapp.com/callback'
   }
 
   // https://powerful-chamber-64882.herokuapp.com/callback
